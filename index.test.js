15ecee08c20faceb0ddacdc2e587adc3fb793e3e
Grayson Deiley <Deiley921@gmail.com>
2025-11-19 11:43:31 -0500

    first commit

---
+++ index.test.js
@@ -0,0 +1,10 @@
+const { add, subtract } = require('./index');
+
+test('adds numbers correctly', () => {
+    expect(add(2, 3)).toBe(5);
+});
+test('subtracts numbers correctly', () => {
+    expect(subtract(5, 3)).toBe(2);
+
+});
+
